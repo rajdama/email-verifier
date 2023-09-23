243dc85d391ebb5b3e073a2870fa7d944f71224a
raj <rajdama1729@gmail.com>
2023-09-23 11:29:45 +0530

    email verifier completed

--- main.go
+++ main.go
@@ -52,4 +52,19 @@ func checkDomain(domain string) {
 		}
 	}
 
+	dmarcRecords, err := net.LookupTXT("_dmarc." + domain)
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
+
+	for _, record := range dmarcRecords {
+		if strings.HasPrefix(record, "v=DMARC1") {
+			hasDMARC = true
+			dmarcRecord = record
+			break
+		}
+	}
+
+	fmt.Printf("%v %v %v %v %v %v", domain, hasMX, hasSPF, spfRecord, hasDMARC, dmarcRecord)
+
 }
